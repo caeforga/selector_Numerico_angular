e443aa3cd0733cce02cabe094c9713faff21ac63
carlos <carlos_ortiz97@gmail.com>
2021-10-23 11:37:35 -0500

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'limites';
+  @ViewChild('selector1') selector1!: SelectornumericoComponent;
+  @ViewChild('selector2') selector2!: SelectornumericoComponent;
+
+  fijarSelector1 (valor: number) {
+    this.selector1.fijar(valor);
+  }
+
+  fijarSelector2 (valor: number) {
+    this.selector2.fijar(valor);
+  }
 }
